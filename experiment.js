0bc8230e1d9977a1e4a873b66e86724c4e14f32a
nybbdo2026 <kk17104408@gmail.com>
2026-05-05 18:14:54 +0530

    Add files via upload

--- experiment.js
+++ experiment.js
@@ -2146,11 +2146,11 @@ console.log("✅ Cleaned trials count:", allData.length);
 
       console.log("✅ Firebase write successful. Key:", snapshot.key);
 
-      window.location.href = `https://www.rdsecured.com/return?inbound_code=1000&rid=${external_id}`;
+      window.location.href = `https://www.rdsecured.com/return?inbound_code=1000&id=${external_id}`;
     } catch (e) {
       console.error("❌ Firebase write failed:", e);
       setTimeout(() => {
-        window.location.href = `https://www.rdsecured.com/return?inbound_code=1000&rid=${external_id}`;
+        window.location.href = `https://www.rdsecured.com/return?inbound_code=1000&id=${external_id}`;
       }, 3000);
     }
   }
